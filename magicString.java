a2dc1e350fd50fd98ef24a362355a6668e2a8aeb
GagaAutom <1049340665@qq.com>
2020-05-20 22:01:03 +0800

    递归——神奇字符矩阵（振兴中华）问题

--- magicString.java
+++ magicString.java
@@ -1,4 +1,13 @@
 
 public class magicString {
-
+	public static int conbination(int x,int y) {
+		if(x == 0)
+			return 1;
+		if(y == 0)
+			return 1;
+		return conbination(x-1,y)+conbination(x,y-1);
+	}
+	public static void main(String[] args) {
+		System.out.println(conbination(5,4));
+	}
 }

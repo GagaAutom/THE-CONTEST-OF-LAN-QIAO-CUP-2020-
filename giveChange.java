e379730cdf03f2c3f11db814b9bc07253e634eac
GagaAutom <1049340665@qq.com>
2020-05-20 15:41:36 +0800

    递归——找钱问题

--- giveChange.java
+++ giveChange.java
@@ -1,15 +1,15 @@
 
 public class giveChange {
 	public static int change(int m,int n) {
-		if(m==0)
-			return 1;
-		if(n==0)
-			return 0;
 		if(m<n)
 			return 0;
+		if(m<=1)
+			return 1;
+		if(n==0)
+			return 1;
 		return change(m-1,n)+change(m,n-1);
 	}
 	public static void main(String[] args) {
-		System.out.println(change(5,3));
+		System.out.println(change(2,2));
 	}
 }

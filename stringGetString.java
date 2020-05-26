af6f94c596b869afff4f920e74e46a824b18ff42
GagaAutom <1049340665@qq.com>
2020-05-26 20:09:18 +0800

    排列组合——组合字符重叠

--- stringGetString.java
+++ stringGetString.java
@@ -1,16 +1,25 @@
-import java.util.Arrays;
 
 //从“AABBBCCD”中取3个，有多少取法？
 public class stringGetString {
-	public static void Get(int[] a,int[] time,int index,int goal) {
+	public static void show(int[] time) {
+		for(int i = 0;i<time.length;i++) {
+			for(int j = 0;j<time[i];j++) {
+				System.out.print((char)('A'+i));
+			}
+		}
+		System.out.println();
+	}
+	public static void Get(int[] data,int[] time,int index,int goal) {
 		if(index == time.length) {
-			System.out.println(Arrays.toString(time));
-			return;
+			if(goal == 0)
+				show(time);
+				return;
 		}
-		for(int i = 0;i<=Math.min(a[index], goal);i++) {
+		for(int i = 0;i<= Math.min(data[index], goal);i++) {
 			time[index] = i;
-			Get(a,time,index+1,goal-i);
+			Get(data,time,index+1,goal-i);
 		}
+		time[index] = 0;
 	}
 	public static void main(String[] args) {
 		int[] a = {2,3,2,1};

e71ae23e29d457b27bbe6a2fa816d17f59c66720
GagaAutom <1049340665@qq.com>
2020-06-06 17:20:44 +0800

    进制与整除——公倍数和公约数

--- ConventionsAndMultiples.java
+++ ConventionsAndMultiples.java
@@ -1,15 +1,6 @@
 
 public class ConventionsAndMultiples {
-	static void swap(int a,int b) {
-		int temp = 0;
-		temp = a;
-		a = b;
-		b = temp;
-	}
 	static int gcd(int a,int b) {
-		if(a<b) {
-			swap(a,b);
-		}
 		if(b == 0) {
 			return a;
 		}
@@ -19,7 +10,7 @@ public class ConventionsAndMultiples {
 		return a * b / gcd(a,b);
 	}
 	public static void main(String[] args) {
-		System.out.println("最大公约数为："+gcd(60,45));
+		System.out.println("最大公约数为："+gcd(45,60));
 		System.out.println("最小公倍数为："+lcm(60,45));
 	}
 }

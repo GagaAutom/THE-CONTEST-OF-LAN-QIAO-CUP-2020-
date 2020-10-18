ec46a8efc567160d524cdad6d48e0ffc98487aa7
GagaAutom <1049340665@qq.com>
2020-10-18 13:50:36 +0800

    蓝桥基础练习

---
+++ O1String.java
@@ -0,0 +1,15 @@
+public class O1String {
+    public static void main(String[] args) {
+        for (int w1 = 0;w1<2;w1++){
+            for (int w2 = 0;w2 < 2;w2++){
+                for (int w3 = 0;w3 < 2;w3++){
+                    for (int w4 = 0;w4 < 2;w4++){
+                        for (int w5 = 0;w5 < 2;w5++){
+                            System.out.println(""+w1+w2+w3+w4+w5);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
